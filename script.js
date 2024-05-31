19ea21e1d285c464aae7d4e9e253ac39378f6ae1
jayjvaughn <waveyjxyshop@gmail.com>
2024-05-31 02:19:21 -0400

    troubleshooting, but this is my working jump code with a new background keyframe properties

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const kitty = document.querySelector(".kitty");
+const obstacle = document.querySelector(".obstacle");
 
 function jump(){
     kitty.classList.add("jumpAnimation");
@@ -6,3 +7,11 @@ function jump(){
     kitty.classList.remove("jumpAnimation");  
     }, 500)
 }
+
+setInterval(() => {
+const kittyTop = parseInt(window.getComputedStyle(kitty).getPropertyValue('top'));
+const obstacleLeft = parseInt(window.getComputedStyle(obstacle).getPropertyValue('left'));
+},50);
+
+
+
